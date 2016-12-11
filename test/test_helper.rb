851b9bc7add1bc8a41b6fc9e5c87fd7eebf882ee
Dan Sullivan <dansullivan@gmail.com>
2016-12-11 07:51:40 -0600

    added simplecov to run alongisde coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!('rails')
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -11,3 +11,9 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
