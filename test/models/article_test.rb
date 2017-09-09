8c974edfea33428ab086e512f10ffcd975ad5cc5
Dan Sullivan <dansullivan@gmail.com>
2017-09-09 15:57:29 -0500

    always fail

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the false" do
+     assert false
+   end
 
   test "should not save article without title" do
     article = Article.new
